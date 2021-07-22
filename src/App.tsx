bd61a7bf9e22b5cf17394b8e5d1b906aff5a6c14
Tebyakin Alexey <tebyakin@intellex.ru>
2021-07-22 13:32:53 +0400

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
+import MissionFilter from './Components/MissionFilter/MissionFilter';
+import MissionList from './Components/MissionList/MissionList';
+import { Reducer } from './reducer';
+import { ReducerContext } from './Hooks/useAppReducer'
+
 
 function App() {
+
+  const reducer = useReducer(Reducer, {});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ left: '50%', position: 'absolute',  transform: 'translateX(-50%)', width: '600px'}}>
+        <ReducerContext.Provider value={reducer}>
+          <h1>Launches</h1>
+          <MissionFilter/>
+          <MissionList/>
+        </ReducerContext.Provider>
+      </div>
   );
 }
 
---
+++ src/Components/MissionFilter/MissionFilter.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useMemo } from "react";
+import { useAppReducer } from "../../Hooks/useAppReducer";
+import { selectLaunchSide, selectRocket, loadData } from '../../actions'
+import { LaunchSite, Rocket } from '../../api'
+import styles from './MissionFilter.module.css'
+
+const MissionFilter = () => {
+    const [ state, dispatch ] = useAppReducer();
+
+    useEffect( () => {
+        loadData(dispatch);
+    }, []);
+    
+    
+    const launchSides = useMemo( () => {
+        if (state.data) {
+            const dicLaunchSites: Map<string, LaunchSite> = new Map();
+            for (let item of state.data) {
+                if (!dicLaunchSites.has(item.launch_site.site_id)) {
+                    dicLaunchSites.set(item.launch_site.site_id, item.launch_site);
+                }
+            }
+
+            const launchSites = [...dicLaunchSites.values()];
+            return launchSites.map( i => { return <option value={i.site_id}>{i.site_name}</option>});
+        }
+        
+    }, [state.data]);
+
+    const rockets = useMemo( () => {
+        if (state.data) {
+            const dicRockets: Map<string, Rocket> = new Map();
+
+            for (let item of state.data) {
+                if (!dicRockets.has(item.rocket.rocket_id)) {
+                    dicRockets.set(item.rocket.rocket_id, item.rocket);
+                }
+            }
+
+            const rockets = [...dicRockets.values()];
+            return rockets.map( i => { return <option value={i.rocket_id}>{i.rocket_name}</option>});
+        }
+    }, [state.data]);
+
+    if (!state.data) {
+        return (
+            <h1 style={{ position: 'absolute', top: '50%', left: '50%' }}>Loading...</h1>
+        )
+    }
+    
+    return (
+        <div className={styles.Main}>
+            <div className={styles.ComboContainer}>
+                <span className={styles.ComboFont}>LaunchSite</span>
+                    <select className={styles.Combo} onChange={ (e) => dispatch(selectLaunchSide(e.target.value))}>
+                        {launchSides}
+                    </select>
+                </div>
+            <div className={styles.ComboContainer} style={{marginLeft: 20}}>
+                <span className={styles.ComboFont}>Rocket</span>
+                <select className={styles.Combo} onChange={ (e) => dispatch(selectRocket(e.target.value))} >
+                    {rockets}
+                </select>
+            </div>
+      </div>
+    );
+  }
+  
+  export default MissionFilter;
